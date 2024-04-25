3c328dd818d9321842442f3ff6e55c26c6967a91
FILA <fila090580@gmail.com>
2024-04-25 23:25:25 +0300

    add

---
+++ config/links.py
@@ -0,0 +1,22 @@
+
+
+class Links_360:
+
+    HOST = "https://beta.syncwise360.com"
+    LOGIN_PAGE = f"{HOST}/login"
+    COURSE_MAP_PAGE = f"{HOST}/coursemap"
+    ASSETS_PAGE = f"{HOST}/asset"
+    MESSAGING_PAGE = f"{HOST}/messages"
+    REPORTS_PAGE = f"{HOST}/reports"
+
+    DEVISE_SETUP_PAGE = f"{HOST}/device-setup"
+    FOOD_DRINK_PAGE = f"{HOST}/food-drink"
+    PIN_LOCATIONS_PAGE = f"{HOST}/location"
+    MOBILE_APP_SETTINGS_PAGE = f"{HOST}/mob-app-setting"
+
+    GENERAL_SETTINGS_PAGE = f"{HOST}/general-setting"
+    ASSET_TRACER_PAGE = f"{HOST}/asset-tracker"
+    ADMIN_PRIVILEGES_PAGE = f"{HOST}/admin-privileges"
+    PRINTERS_TERMINALS_PAGE = f"{HOST}/printers-terminals"
+
+    TOURNAMENTS_PAGE = f"{HOST}/tournaments"
---
+++ pages/login_page.py
@@ -0,0 +1,20 @@
+from base.base_page import BasePage
+from config.links import Links_360
+from selenium.webdriver.support import expected_conditions as EC
+
+class LoginPage(BasePage):
+
+    PAGE_URL = Links_360.LOGIN_PAGE
+
+    USERNAME_FIELD = ("xpath", "//input[@id='username']")
+    PASSWORD_FIELD = ("xpath", "//input[@id='password']")
+    BUTTON_LOGIN = ("xpath", "//button[@aria-label='submit']")
+
+    def enter_login(self, login):
+        self.wait.until(EC.element_to_be_clickable(self.USERNAME_FIELD)).send_keys("")
+
+    def enter_password(self, password):
+        self.wait.until(EC.element_to_be_clickable(self.PASSWORD_FIELD)).send_keys("")
+
+    def click_login_button(self):
+        self.wait.until(EC.element_to_be_clickable(self.BUTTON_LOGIN)).click()
