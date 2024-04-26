b8dbbf99469de9eacfba64e878c03a505b364c63
FILA <fila090580@gmail.com>
2024-04-26 23:43:28 +0300

    add

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,6 +2,7 @@ from base.base_page import BasePage
 from config.links import Links_360
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class LoginPage(BasePage):
 
     PAGE_URL = Links_360.LOGIN_PAGE
